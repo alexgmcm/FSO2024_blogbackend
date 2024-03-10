a19a2e595b59928ec21b65be9e522f78481e83a6
Alexander McMurray <alexgmcm@gmail.com>
2024-03-10 17:31:09 +0100

    ex 4.10 done

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app')
 const assert = require('node:assert')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const _ = require('lodash')
 
 const initialBlogs = [
     {
@@ -41,6 +42,22 @@ test('Returns two blogs', async () => {
     const response = await api.get('/api/blogs')
     //console.log(response.body)
     assert.strictEqual(response.body.length,2)
+})
+
+test('contains id field', async () => {
+    const response = await api.get('/api/blogs')
+    //console.log(response.body)
+    const blog = response.body[0]
+    assert(Object.keys(blog).includes("id"))
+})
+
+test('new blog created successfully', async () => {
+    const newBlog = {"title":"posted blog", "author":"test bot","url":"http://test.com","likes":222}
+    const response = await api.post('/api/blogs').send(newBlog).expect(201)
+    const submittedBlog = response.body
+
+    const getNewBlogs = await api.get('/api/blogs')
+    assert(getNewBlogs.body.some((blog) => _.isEqual(blog,submittedBlog) ))
 })
 
 after(async () => {
