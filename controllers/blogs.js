6872394358ddd44b0136feddc5638d7e39612a0d
Alexander McMurray <alexgmcm@gmail.com>
2024-03-24 18:09:47 +0100

    ex 4.17 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 blogsRouter.get('', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user',{ username: 1, name: 1 })
   response.json(blogs)
   })
 
@@ -28,8 +30,13 @@ blogsRouter.get('', async (request, response) => {
   
   blogsRouter.post('/', async (request, response, next) => {
     try{
-    const blog = new Blog(request.body)
+    const users = await User.find({})
+    const user = users[0] // use first user found
+    const blog = new Blog({...request.body, user: user.id })
     const savedBlog = await blog.save()
+    user.blogs=user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
     } catch(exception){
       next(exception)
--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,7 @@ try{
 }})
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1 })
     response.json(users)
 })
 
